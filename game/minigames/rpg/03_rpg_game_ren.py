be680d455f6558f189d86604ab03edc2e74945a9
4Bakers <aidanbaker2187@gmail.com>
2024-06-17 19:36:47 -0400

    lego sound filename fix
    in RPG game

--- game/minigames/rpg/03_rpg_game_ren.py
+++ game/minigames/rpg/03_rpg_game_ren.py
@@ -759,7 +759,7 @@ class Fighters:
 
 
 # Dummy encounter to avoid errors
-encounter = Encounter([], Image("images/bg/black.png"), "audio/legosfx.mp3", 1, "start", "secret")
+encounter = Encounter([], Image("images/bg/black.png"), "audio/sfx_legosfx.mp3", 1, "start", "secret")
 
 # Indicator types
 IndicatorType = Literal["heal", "damage", "stat_up", "stat_down", "confused", "unconfused", "none"]
